ae8504f6649191e8c24d0d277fde45337e38caa7
SevenKhan <muammer345253@gmail.com>
2025-11-07 20:58:35 +0300

    Add tracker.py safe version without secrets

---
+++ tracker.py
@@ -0,0 +1,40 @@
+import os
+from github import Github, Auth
+import json
+
+TOKEN = os.environ.get("GITHUB_PAT")
+if not TOKEN:
+    raise ValueError("GITHUB_PAT environment variable not set!")
+
+USERNAME = "SevenKhan"
+g = Github(auth=Auth.Token(TOKEN))
+user = g.get_user(USERNAME)
+
+profile_info = {
+    "login": user.login,
+    "name": user.name,
+    "bio": user.bio,
+    "public_repos": user.public_repos,
+    "followers": user.followers,
+    "following": user.following
+}
+
+repos_list = []
+for repo in user.get_repos():
+    repos_list.append({
+        "name": repo.name,
+        "stars": repo.stargazers_count,
+        "forks": repo.forks_count,
+        "open_issues": repo.open_issues_count,
+        "private": repo.private
+    })
+
+output = {
+    "profile": profile_info,
+    "repos": repos_list
+}
+
+with open("output.json", "w") as f:
+    json.dump(output, f, indent=4)
+
+print("✅ Data saved to output.json")
